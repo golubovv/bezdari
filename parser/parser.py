af6d452b6087edf8eae5e7f1afe63a6f90802952
Andrey Shtamov <shtamov07@bk.ru>
2024-03-29 20:47:17 +0500

    get img

--- parser/parser.py
+++ parser/parser.py
@@ -14,7 +14,7 @@ def test_parse():
     src = req.text
 
     soup = BeautifulSoup(src, "lxml")
-    all_class_event = soup.find_all(class_="sueta-afisha-text")
+    all_class_event = soup.find_all(class_="sueta-afisha-element")
 
     all_event_list = []
     for item in all_class_event:
@@ -22,13 +22,15 @@ def test_parse():
         item_date = item.find(class_="sueta-afisha-date").text
         item_title = item.find(class_="sueta-afisha-title").text
         item_content = item.find(class_="sueta-afisha-description").text
+        item_img = "https://iceshow-perm.ru/" + str(item.find("img").get("src"))
         item_date = item_date.split(",")
 
         dict_event['title'] = item_title
         dict_event['date'] = item_date[0]
         dict_event['content'] = item_content
+        dict_event['img'] = item_img
         dict_event['location'] = item_date[1]
 
         all_event_list.append(dict_event)
 
-        return all_event_list
+    return all_event_list
